9d8fdc9204cdaa80bfa12f73df9aea093b627eaa
Thomas <thomas.moreau.master@gmail.com>
2017-02-02 02:50:00 +0100

    Structure init_scrutin

--- chaincode/marbles_chaincode.go
+++ chaincode/marbles_chaincode.go
@@ -133,9 +133,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return res, err
 	} else if function == "write" { //writes a value to the chaincode state
 		return t.Write(stub, args)
-	} else if function == "init_marble" { //create a new marble
-		return t.init_marble(stub, args)
-	} else if function == "init_scrutin" { //create a new marble
+	} else if function == "marble" { //create a new marble
 		return t.init_marble(stub, args)
 	} else if function == "set_user" { //change owner of a marble
 		res, err := t.set_user(stub, args)
