08078df58508daf5baf5d5318fe1209dac440ad3
Thomas <thomas.moreau.master@gmail.com>
2017-02-06 17:17:54 +0100

    read opening

--- cs/s_chaincode.go
+++ cs/s_chaincode.go
@@ -186,7 +186,7 @@ func (t *SimpleChaincode) read_opened(stub shim.ChaincodeStubInterface, args []s
 
 	valAsbytes, err := stub.GetState(openScrutinStr)
 	if err != nil {
-		return err, errors.New("Failed to get marble index")
+		return nil, errors.New("Failed to get marble index")
 	}
 
 	return valAsbytes, nil //send it onward
